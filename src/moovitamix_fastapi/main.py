9066dd9d990c4468b90d9baf1fda9fed289a0c2e
Sam Kryszto <sam.kryszto@gmail.com>
2025-01-19 11:38:31 -0500

    local import

--- src/moovitamix_fastapi/main.py
+++ src/moovitamix_fastapi/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Query
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.responses import RedirectResponse
 from fastapi_pagination import Page, add_pagination, paginate
-from generate_fake_data import FakeDataGenerator
+from .generate_fake_data import FakeDataGenerator
 
 Page = Page.with_custom_options(
     size=Query(100, ge=1, le=100),
